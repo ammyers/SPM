ce028e1d1ae76f3cbb11a44f60dad11536a3060b
Luke Janssen <ljanssen@pugetsound.edu>
2014-10-27 12:41:00 -0700

    Includes Schema file and now successfully can migrate and seed dbs

--- db/migrate/20141027191856_create_studies_table.rb
+++ db/migrate/20141027191856_create_studies_table.rb
@@ -1,12 +1,12 @@
 class CreateStudiesTable < ActiveRecord::Migration
   def up
   	create_table :studies do |s|
-      p.string :title
-      p.string :description
-      p.int :max_participants
-      p.string :special_instructions
+      s.string :title
+      s.string :description
+      s.integer :max_participants
+      s.string :special_instructions
 
-      p.timestamps
+      s.timestamps
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-bobby = Person.new(:id => 0, :first_name => 'Bobby', :last_name => 'Carpenter-Participant', :email => 'bcarpenter@ups.edu', :password => 'chuck', :role => 'participant', :classes => [201])
+bobby = Person.new(:first_name => 'Bobby', :last_name => 'Carpenter-Participant', :email => 'bcarpenter@ups.edu', :password => 'chuck', :role => 'participant')
 
-sally = Person.new(:id => 1, :first_name => 'Sally', :last_name => 'Winthrop-Researcher', :email => 'swinthrop@ups.edu', :password => 'wendy', :role => 'researcher')
+sally = Person.new(:first_name => 'Sally', :last_name => 'Winthrop-Researcher', :email => 'swinthrop@ups.edu', :password => 'wendy', :role => 'researcher')
     	  
-robin = Person.new(:id => 2, :first_name => 'Robin', :last_name => 'Dorn-Admin', :email => 'rdorn@ups.edu', :password => 'password', :role => 'admin')
+robin = Person.new(:first_name => 'Robin', :last_name => 'Dorn-Admin', :email => 'rdorn@ups.edu', :password => 'password', :role => 'admin')
 
 robin.save
 sally.save
 bobby.save
 
-study = Study.new(:id => 101, :title => 'Testing the presence of studies and people', :description => 'Can we make this database work? Can we access the data?', :max_participants => 40, :special_instructions => 'none', :date_time => ['10/18 4:00 PM'], :location => ['WEY 230'], :participants => [0], :researchers => [1])
-
-bobby.studies = [101]
-sally.create_studies = [101]
+study = Study.new(:title => 'Testing the presence of studies and people', :description => 'Can we make this database work? Can we access the data?', :max_participants => 40, :special_instructions => 'none')
 
 study.save
-bobby.save
-sally.save

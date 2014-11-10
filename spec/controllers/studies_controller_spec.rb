1c7259d6819c59ec51f55f7a212fe119ef319131
Adam Myers <amyers@pugetsound.edu>
2014-11-09 17:08:33 -0800

    Adds inital factory girl stuff.

--- spec/controllers/studies_controller_spec.rb
+++ spec/controllers/studies_controller_spec.rb
@@ -2,8 +2,19 @@ require 'spec_helper'
 
 RSpec.describe StudiesController, :type => :controller do
   describe 'creating study' do  
-    it "expects redirect to studies with a notice of successful save" do
-
+		context "with studies in the database" do
+			before :each do 
+				@test1 = FactoryGirl.create(:study, :title => 'test1')
+				#@test1.studies << FactoryGirl.create(:study, :points=>1) #building fake models, not stubbing!
+				@test2 = FactoryGirl.create(:study, :title => 'test2')
+				#@jill.user_stories << FactoryGirl.create(:user_story, :points=>3)
+			end
+			describe "busiest developer" do
+				it 'assigns the busiest developer' do
+					get :index
+					#expect(assigns(:busiest)).to eq(@jill)
+				end
+			end
  	 end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,8 @@ SimpleCov.start
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'factory_girl'
 
 RSpec.configure do |config|
-
+	config.include FactoryGirl::Syntax::Methods	
 end

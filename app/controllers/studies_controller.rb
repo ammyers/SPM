7f6f6b2e64c2198813853cf14c3cafdd4e47e439
Luke Janssen <ljanssen@pugetsound.edu>
2014-11-09 19:31:53 -0800

    Makes the researcher name appear in the all studies page - sets up some user variables on pages.

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -1,6 +1,7 @@
 class StudiesController < ApplicationController
 
   def show
+    @me = get_user
     id = params[:id] # retrieve study ID from URI route
     @study = Study.find(id) # look up study by unique ID
     # will render app/views/studys/show.<extension> by default
@@ -12,20 +13,24 @@ class StudiesController < ApplicationController
   end
 
   def new
+    @me = get_user
     # default: render 'new' template
   end
 
   def create
+    @me = get_user
     @study = Study.create!(params[:study])
     flash[:notice] = "#{@study.title} was successfully created."
     redirect_to studies_path
   end
 
   def edit
+    @me = get_user
     @study = Study.find params[:id]
   end
 
   def update
+    @me = get_user
     @study = Study.find params[:id]
     @study.update_attributes!(params[:study])
     flash[:notice] = "#{@study.title} was successfully updated."
@@ -33,6 +38,7 @@ class StudiesController < ApplicationController
   end
 
   def destroy
+    @me = get_user
     @study = Study.find(params[:id])
     @study.destroy
     flash[:notice] = "Study '#{@study.title}' deleted."
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-bobby = User.create(:first_name => 'Bobby', :last_name => 'Carpenter-Participant', :email => 'bcarpenter@ups.edu', :password => 'chuck', :role => 'participant')
-sally = User.create(:first_name => 'Sally', :last_name => 'Winthrop-Researcher', :email => 'swinthrop@ups.edu', :password => 'wendy', :role => 'researcher')   	  
-robin = User.create(:first_name => 'Robin', :last_name => 'Dorn-Admin', :email => 'rdorn@ups.edu', :password => 'password', :role => 'admin')
+bobby = User.create(:first_name => 'Bobby', :last_name => 'Carpenter', :email => 'bcarpenter@ups.edu', :password => 'chuck', :role => 'participant')
+sally = User.create(:first_name => 'Sally', :last_name => 'Winthrop', :email => 'swinthrop@ups.edu', :password => 'wendy', :role => 'researcher')   	  
+robin = User.create(:first_name => 'Robin', :last_name => 'Dorn', :email => 'rdorn@ups.edu', :password => 'password', :role => 'admin')
 no_studies = User.create(:first_name => 'Damon', :last_name => 'Templeton', :email => 'da', :password => 'password', :role => 'participant')
 
 db_study = Study.create(:title => 'Test Study', :description => 'Can we make this database work? Can we access the data?', :max_participants => 40);

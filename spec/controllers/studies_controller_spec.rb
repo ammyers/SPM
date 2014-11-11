30442171e4b86c6450fa8e55fe95c7acc5ab8dbd
Adam Myers <amyers@pugetsound.edu>
2014-11-10 18:37:06 -0800

    Adds some study model rspec tests that pass. Updates spec helper to use should syntax

--- spec/controllers/studies_controller_spec.rb
+++ spec/controllers/studies_controller_spec.rb
@@ -1,21 +1,9 @@
-#rspec for testing models
 require 'spec_helper'
 
 RSpec.describe StudiesController, :type => :controller do
-  describe 'creating study' do  
-		context "with studies in the database" do
-			before :each do 
-				@test1 = FactoryGirl.create(:study, :title => 'test1')
-				#@test1.studies << FactoryGirl.create(:study, :points=>1) #building fake models, not stubbing!
-				@test2 = FactoryGirl.create(:study, :title => 'test2')
-				#@jill.user_stories << FactoryGirl.create(:user_story, :points=>3)
-			end
-			describe "busiest developer" do
-				it 'assigns the busiest developer' do
-					get :index
-					#expect(assigns(:busiest)).to eq(@jill)
-				end
-			end
- 	 end
+  describe 'creating ....' do  
+    it '....'
+    it '....'
+    it '....'
   end
 end
---
+++ spec/models/study_spec.rb
@@ -0,0 +1,34 @@
+#rspec for testing models
+require 'spec_helper'
+
+RSpec.describe Study, :type => :model do
+
+	describe '#title and #description' do
+
+	it "is invalid without a title" do
+		title = FactoryGirl.build(:study, :title => nil) #test object
+		expect(title).not_to be_valid #the test!
+	end
+
+	it "is invalid without a description" do
+		description = FactoryGirl.build(:study, :description => nil) #test object
+		expect(description).not_to be_valid #the test!
+	end
+end
+
+	describe '#createstudy' do
+
+	it "creates a study" do
+		study = FactoryGirl.build(:study, :title => "Testing", :description => "This is a description",
+			:max_participants => "60", :duration => "30")
+		study.title.should == "Testing"
+		study.description.should == "This is a description"
+		study.max_participants.should == 60
+		study.duration == 30
+	end
+end
+
+end
+
+
+
